282376a05235f3a75a502bd99d2e0423c436d98d
darsh <darsh@DARSHIT>
2025-09-24 18:20:08 -0400

    Add project files.

---
+++ darshitportfolio/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace darshitportfolio
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
